0e46631cebace2343de429f4c8c692f43d103b63
Henke <70940165+92Hank@users.noreply.github.com>
2023-03-27 17:08:26 +0200

    added react router

--- env-demo/src/App.tsx
+++ env-demo/src/App.tsx
@@ -1,57 +1,32 @@
-import React from 'react';
-import logo from './logo.svg';
-import { Counter } from './features/counter/Counter';
+import React, { useState } from 'react';
 import './App.css';
+import { Container, createTheme, ThemeProvider } from '@mui/material';
+import Header from './features/header/Header';
+import { Outlet } from 'react-router-dom';
 
 function App() {
+  const [darkMode, setDarkMode] = useState(false);
+  const paletteType = darkMode ? "dark" : "light";
+  const theme = createTheme({
+    palette: {
+      mode: paletteType,
+      background: {
+        default: paletteType === "light" ? "#eaeaea" : "#121212",
+      },
+    },
+  });
+
+  function handleThemeChange() {
+    setDarkMode(!darkMode);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <Counter />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <span>
-          <span>Learn </span>
-          <a
-            className="App-link"
-            href="https://reactjs.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux-toolkit.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux Toolkit
-          </a>
-          ,<span> and </span>
-          <a
-            className="App-link"
-            href="https://react-redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React Redux
-          </a>
-        </span>
-      </header>
-    </div>
+    <ThemeProvider theme={theme}>
+      <Header darkMode={darkMode} handleThemeChange={handleThemeChange} />
+      <Container>
+        <Outlet />
+      </Container>
+    </ThemeProvider>
   );
 }
 
--- env-demo/src/features/counter/Counter.tsx
+++ env-demo/src/features/counter/Counter.tsx
@@ -1,6 +1,6 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
-import { useAppSelector, useAppDispatch } from '../../app/hooks';
+import { useAppSelector, useAppDispatch } from "../../app/hooks";
 import {
   decrement,
   increment,
@@ -8,61 +8,86 @@ import {
   incrementAsync,
   incrementIfOdd,
   selectCount,
-} from './counterSlice';
-import styles from './Counter.module.css';
+} from "./counterSlice";
+import styles from "./Counter.module.css";
+import { Button, Container, TextField, Typography } from "@mui/material";
 
-export function Counter() {
+export default function Counter() {
   const count = useAppSelector(selectCount);
   const dispatch = useAppDispatch();
-  const [incrementAmount, setIncrementAmount] = useState('2');
+  const [incrementAmount, setIncrementAmount] = useState("2");
 
   const incrementValue = Number(incrementAmount) || 0;
 
   return (
-    <div>
-      <div className={styles.row}>
-        <button
-          className={styles.button}
-          aria-label="Decrement value"
-          onClick={() => dispatch(decrement())}
-        >
-          -
-        </button>
-        <span className={styles.value}>{count}</span>
-        <button
-          className={styles.button}
-          aria-label="Increment value"
-          onClick={() => dispatch(increment())}
-        >
-          +
-        </button>
-      </div>
-      <div className={styles.row}>
-        <input
-          className={styles.textbox}
-          aria-label="Set increment amount"
-          value={incrementAmount}
-          onChange={(e) => setIncrementAmount(e.target.value)}
-        />
-        <button
-          className={styles.button}
-          onClick={() => dispatch(incrementByAmount(incrementValue))}
-        >
-          Add Amount
-        </button>
-        <button
-          className={styles.asyncButton}
-          onClick={() => dispatch(incrementAsync(incrementValue))}
-        >
-          Add Async
-        </button>
-        <button
-          className={styles.button}
-          onClick={() => dispatch(incrementIfOdd(incrementValue))}
-        >
-          Add If Odd
-        </button>
-      </div>
-    </div>
+    <>
+      <Container
+        style={{
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "center",
+          flexDirection: "column",
+        }}
+      >
+        <Container style={{ marginBottom: "25px" }}>
+          <Button
+            aria-label="Decrement value"
+            variant="outlined"
+            style={{
+              padding: "15px",
+              background: "lightblue",
+              fontStyle: "bold",
+            }}
+            onClick={() => dispatch(decrement())}
+          >
+            -
+          </Button>
+          <Typography style={{ padding: "10px", fontSize: 28 }}>
+            {count}
+          </Typography>
+          <Button
+            aria-label="Increment value"
+            variant="outlined"
+            style={{
+              padding: "15px",
+              background: "lightblue",
+              fontStyle: "bold",
+            }}
+            onClick={() => dispatch(increment())}
+          >
+            +
+          </Button>
+        </Container>
+        <Container className={styles.row}>
+          <TextField
+            id="outlined-basic"
+            label="Set increment amount"
+            variant="outlined"
+            value={incrementAmount}
+            onChange={(e: { target: { value: any } }) =>
+              setIncrementAmount(e.target.value)
+            }
+          />
+          <Button
+            className={styles.button}
+            onClick={() => dispatch(incrementByAmount(incrementValue))}
+          >
+            Add Amount
+          </Button>
+          <Button
+            className={styles.asyncButton}
+            onClick={() => dispatch(incrementAsync(incrementValue))}
+          >
+            Add Async
+          </Button>
+          <Button
+            className={styles.button}
+            onClick={() => dispatch(incrementIfOdd(incrementValue))}
+          >
+            Add If Odd
+          </Button>
+        </Container>
+      </Container>
+    </>
   );
 }
