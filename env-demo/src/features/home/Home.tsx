13856e01f9e32189de6ed85125028bc69ea2a0cc
Henke <70940165+92Hank@users.noreply.github.com>
2023-04-02 16:14:48 +0200

    create env

--- env-demo/src/features/home/Home.tsx
+++ env-demo/src/features/home/Home.tsx
@@ -1,5 +1,5 @@
 import { Typography } from "@mui/material";
 
 export default function Home() {
-  return <Typography variant="h2">Home Page test</Typography>;
+  return <Typography variant="h2">Home Page</Typography>;
 }
