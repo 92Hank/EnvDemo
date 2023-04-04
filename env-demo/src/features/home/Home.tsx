0a17eb245f31c80e5dd6ab9b192e84636f7fd281
Henke <70940165+92Hank@users.noreply.github.com>
2023-04-04 09:23:52 +0200

    new yml file

--- env-demo/src/features/home/Home.tsx
+++ env-demo/src/features/home/Home.tsx
@@ -24,8 +24,8 @@ export default function Home() {
     <Container
       sx={{ flex: 1, flexDirection: "column", justifyContent: "center" }}
     >
-      <Typography variant="h2">Home Page</Typography>
-      <Typography variant="h4">{githubTag.slice(0, -6)}</Typography>
+      <Typography variant="h2">Home Page!</Typography>
+      <Typography variant="h4">{githubTag ? githubTag?.slice(0, -6) : "develop"}</Typography>
     </Container>
   );
 }
--- env-demo/src/gitInfo.ts
+++ env-demo/src/gitInfo.ts
@@ -13,7 +13,7 @@ const execSyncWrapper = (command: any) => {
   return output;
 }
 
-const main = () => {
+export default function printGitTag() {
   let gitTag = execSyncWrapper("git describe --tags --abbrev=0");
   var gitTagWithDate = execSyncWrapper(`git log -1 --format=%ai --date=short ${gitTag}`);
   // let gitTag = execSyncWrapper("git tag --sort=v:refname");
@@ -30,5 +30,23 @@ const main = () => {
   fs.writeFileSync(filePath, fileContents);
   console.log(`Wrote the following contents to ${filePath}${fileContents}`);
 };
+
+const main = () => {
+  let gitTag = execSyncWrapper("git describe --tags --abbrev=0");
+  let gitTagWithDate = execSyncWrapper(`git log -1 --format=%ai --date=short ${gitTag}`);
+  // let gitTag = execSyncWrapper("git tag --sort=v:refname");
+  // let gitTag = execSyncWrapper("git describe --tags --abbrev=0");
+
+  const obj = {
+    gitTag,
+    gitTagWithDate
+  };
+
+  const filePath = path.resolve('src', 'gitTagInfo.json');
+  const fileContents = JSON.stringify(obj);
+
+  fs.writeFileSync(filePath, fileContents);
+  console.log(`Wrote the following contents to ${filePath}${fileContents}`);
+}
 
 main();
