dfef06ecdc603ac460947a18177c3d6fbea35645
Henke <henrik.g.kristensson@gmail.com>
2023-04-05 13:49:23 +0200

    test

---
+++ env-demo/src/gitTag.js
@@ -0,0 +1,31 @@
+import fs from 'fs';
+import path from 'path';
+import { execSync } from 'child_process';
+
+const execSyncWrapper = (command) => {
+    let output = null;
+    try {
+        output = execSync(command).toString().trim().split(/\r?\n/);
+    } catch (error) {
+        console.error(error);
+    }
+    return output;
+}
+
+const main = () => {
+    let gitTag = execSyncWrapper("git describe --always --tags --abbrev=0");
+    let gitTagWithDate = execSyncWrapper(`git log -1 --format=%ai --date=short ${gitTag}`);
+
+    const obj = {
+        gitTag,
+        gitTagWithDate
+    };
+
+    const filePath = path.resolve('env-demo/src', 'gitTagVersion.json');
+    const fileContents = JSON.stringify(obj);
+
+    fs.writeFileSync(filePath, fileContents);
+    console.log(`Wrote the following contents to ${filePath}${fileContents}`);
+}
+
+main();
