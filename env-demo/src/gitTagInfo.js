666ed13a33fa22555ca2080a613cc3c3f830633c
Henke <henrik.g.kristensson@gmail.com>
2023-04-12 18:58:27 +0200

    test

--- env-demo/src/gitTagInfo.js
+++ env-demo/src/gitTagInfo.js
@@ -13,46 +13,6 @@ const execSyncWrapper = (command) => {
     return output;
 }
 
-// export default function printGitTag(tag) {
-//     // let gitTag = execSyncWrapper("git describe --tags --abbrev=0");
-//     // var gitTagWithDate = execSyncWrapper(`git log -1 --format=%ai --date=short ${gitTag}`);
-//     // let gitTag = execSyncWrapper("git tag --sort=v:refname");
-//     // let gitTag = execSyncWrapper("git describe --tags --abbrev=0");
-//     let gitTag = null;
-//     //let gitTagWithDate = null;
-//     gitTag = tag ? tag : "test"
-//     //gitTagWithDate = github.event.repository.updated_at
-
-//     const obj = {
-//         gitTag,
-//         //gitTagWithDate
-//     };
-
-//     const filePath = path.resolve('env-demo/src', 'gitTagVersion.json');
-//     const fileContents = JSON.stringify(obj);
-
-//     fs.writeFileSync(filePath, fileContents);
-//     console.log(`Wrote the following contents to ${filePath}${fileContents}`);
-// };
-
-// module.exports = () => {
-//     let gitTag = execSyncWrapper("git describe --tags --abbrev=0");
-//     var gitTagWithDate = execSyncWrapper(`git log -1 --format=%ai --date=short ${gitTag}`);
-//     // let gitTag = execSyncWrapper("git tag --sort=v:refname");
-//     // let gitTag = execSyncWrapper("git describe --tags --abbrev=0");
-
-//     const obj = {
-//         gitTag,
-//         gitTagWithDate
-//     };
-
-//     const filePath = path.resolve('src', 'gitTagVersion.json');
-//     const fileContents = JSON.stringify(obj);
-
-//     fs.writeFileSync(filePath, fileContents);
-//     console.log(`Wrote the following contents to ${filePath}${fileContents}`);
-// }
-
 const main = () => {
     let gitTag = execSyncWrapper("git describe --always --tags --abbrev=0");
     let gitTagWithDate = execSyncWrapper(`git log -1 --format=%ai --date=short ${gitTag}`);
